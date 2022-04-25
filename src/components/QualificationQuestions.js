a6aa206f5189af9b8a80ca2deacdbd2d88062cfd
Titus Njiru <njirutitus@gmail.com>
2022-04-25 17:27:48 +0300

    update qualification questions to submit form

--- src/components/QualificationQuestions.js
+++ src/components/QualificationQuestions.js
@@ -1,13 +1,37 @@
+import { AssessmentQuestions } from "./AssessmentQuestions";
+import { addNewDocument } from "./Firestore";
+
 export function DisplayQualificationQuestions(questions) {
-  let form = '<form id="form" action="" method="post">';
+  document.addEventListener("submit", (e) => {
+    if (e.target.id === "qual_form") {
+      e.preventDefault();
+      const qualData = new FormData(e.target);
+      const data = {};
+      for (let [key, value] of qualData.entries()) {
+        data[key] = value;
+      }
+      addNewDocument("qualificationAnswers", data);
+      AssessmentQuestions();
+    }
+  });
+
+  let form =
+    '<form id="qual_form" method="post"><h3>Qualification Questions</h3>';
   questions.forEach((question) => {
     form += "<div>";
     form +=
       '<label for="' + question.id + '">' + question.question + "</label>";
-    form += '<input type="radio" name="' + question.id + '" />Yes';
-    form += '<input type="radio" name="' + question.id + '" />NO';
+    const options = question.options;
+    Object.entries(options).forEach(([key, value]) => {
+      if (question.questionType === "radio")
+        form += `<input type="${question.questionType}" name="${question.id}" value="${key}"/>${key}`;
+      else
+        form += `<input type="${question.questionType}" name="${
+          question.id + "_" + key
+        }" value="${key}"/>${key}`;
+    });
     form += "</div>";
   });
   document.getElementById("root").innerHTML =
-    form + '<input type="submit" value="submit"</form>';
+    form + '<input type="submit" value="submit"></form>';
 }
