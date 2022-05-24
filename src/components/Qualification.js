e4de186ec69f5d991a6f0f125fd3c6b458c84482
Titus Njiru <njirutitus@gmail.com>
2022-05-24 18:50:41 +0300

    update ui

--- src/components/Qualification.js
+++ src/components/Qualification.js
@@ -32,52 +32,163 @@ export async function fetchQuestions() {
   return questions;
 }
 
+function questDiv() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "row my-5");
+  return div;
+}
+
+function questInnerDiv() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "row my-3 justify-content-center");
+  return div;
+}
+
+function colDiv() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "col-6 bg-light rounded-2");
+  return div;
+}
+
+function questNoH4(count) {
+  let h4 = document.createElement("h4");
+  h4.setAttribute("class", "text-center my-2");
+  h4.innerHTML = `Question ${count}`;
+  return h4;
+}
+
+function questTextP(question) {
+  let p = document.createElement("p");
+  p.setAttribute("class", "text-center");
+  p.innerHTML = question;
+  return p;
+}
+
+function accordionDiv() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "accordion accordion-flush my-2");
+  return div;
+}
+
+function accordionItem() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "accordion-item");
+  return div;
+}
+
+function accordionHeader(questionID) {
+  let h2 = document.createElement("h2");
+  h2.setAttribute("class", "accordion-header");
+  let button = document.createElement("button");
+  button.setAttribute("class", "accordion-button collapsed mx-2");
+  button.setAttribute("type", "button");
+  button.setAttribute("data-bs-toggle", "collapse");
+  button.setAttribute("data-bs-target", `#${questionID}`);
+  button.setAttribute("aria-expanded", "false");
+  button.setAttribute("aria-controls", "collapseone");
+  button.innerHTML = "Options";
+  h2.appendChild(button);
+  return h2;
+}
+
+function accordionCollapse(questionID) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "accordion-collapse collapse");
+  div.setAttribute("id", questionID);
+  div.setAttribute("aria-labelledby", "collapseone");
+  return div;
+}
+
+function accordionBody() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "accordion-body");
+  return div;
+}
+
+function renderOptions(options, questionID) {
+  let div = accordionBody();
+  options.forEach((option) => {
+    let radio = document.createElement("input");
+    radio.setAttribute("type", "radio");
+    radio.setAttribute("name", questionID);
+    radio.setAttribute("value", option.id);
+    radio.setAttribute("data-law", option.law);
+    radio.setAttribute("data-category", option.category);
+    radio.setAttribute("id", option.id);
+    radio.setAttribute("class", "form-check-input");
+    let label = document.createElement("label");
+    label.setAttribute("class", "form-check-label");
+    label.innerHTML = option.option;
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "form-check");
+    div2.appendChild(radio);
+    div2.appendChild(label);
+    div.appendChild(div2);
+  });
+  return div;
+}
+
+/* 
+  questDiv
+    questInnerDiv
+      colDiv
+        questNoH4
+        questTextP
+        accordion
+          accordionItem
+            accordionHeader
+            accordionCollapse
+              acccordionBody
+                renderOptions
+
+*/
+
 function options(questions) {
   let form = document.getElementById("qual_form");
   form.addEventListener("submit", (e) => handleSubmit(e));
-  let div;
-  let label;
+  let count = 0;
   questions.forEach((question) => {
-    div = document.createElement("div");
-    label = document.createElement("label");
-    div.setAttribute("id", question.id);
-    label.innerHTML = question.question;
-    form.appendChild(div);
-    div.appendChild(label);
-    div.setAttribute("aria-busy", "true");
-    let qualQuestion = `<div class="row my-3 justify-content-center">
-    <div class="col-6 bg-light rounded-2">
-      <h4 class="text-center my-2">Question 1</h4>
-      <!-- question text -->
-      <p class="text-center">Do you store personal data?</p>
-        <!-- collapsable options button -->
-      <div class="accordion accordion-flush my-2">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingone">
-            <button class="accordion-button collapsed mx-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapseone" aria-expanded="false" aria-controls="collapseone">
-              Options
-            </button>
-          </h2>
-          <!-- options  -->
-          <div id="collapseone" class="accordion-collapse collapse" aria-labelledby="headingone" data-bs-parent="#accordionExample">
-            <div class="accordion-body">`;
+    count++;
+    const quest = questDiv();
+    form.appendChild(quest);
+    const questInner = questInnerDiv();
+    quest.appendChild(questInner);
+    const col = colDiv();
+    col.appendChild(questNoH4(count));
+    col.appendChild(questTextP(question.question));
+    questInner.appendChild(col);
+    let accord = accordionDiv();
+    col.appendChild(accord);
+    let accordItem = accordionItem();
+    accord.appendChild(accordItem);
+    accordItem.appendChild(accordionHeader(question.id));
+    let accordColla = accordionCollapse(question.id);
+    accordItem.appendChild(accordColla);
     getOptions(question.id).then((snapshot) => {
-      div.setAttribute("aria-busy", "false");
       const options = snapshot.docs.map((doc) => {
         return {
           id: doc.id,
           ...doc.data(),
         };
       });
-      options.forEach((option) => {
-        renderOption(option, question.id);
-      });
+      document
+        .getElementById(question.id)
+        .appendChild(renderOptions(options, question.id));
     });
   });
-  const input = document.createElement("button");
-  input.setAttribute("type", "submit");
-  input.innerHTML = "Submit";
-  form.appendChild(input);
+  const saveButton = document.createElement("button");
+  saveButton.id = "save-progress";
+  saveButton.setAttribute("class", "btn btn-secondary w-25 align-self-center");
+  saveButton.innerText = "Save Progress";
+  saveButton.addEventListener("click", () => saveProgess());
+  const submitButton = document.createElement("button");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute(
+    "class",
+    "btn btn-secondary w-25 align-self-center"
+  );
+  submitButton.innerHTML = "Submit";
+  form.appendChild(submitButton);
 }
 
 function renderOption(option, questionID) {
@@ -103,6 +214,10 @@ export async function getOptions(questionID) {
   const snapshot = await getDocs(optionsQuery);
 
   return snapshot;
+}
+
+function saveProgess() {
+  // TODO
 }
 
 function handleSubmit(e) {
